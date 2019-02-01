222a3000d56ec44d775ee8e877f90c3468900d4a
Tommy Huang <tommyhuang999@gmail.com>
2019-02-01 12:48:19 -0500

    added folder for output

---
+++ pizza/test1.py
@@ -0,0 +1,9 @@
+
+inputFile = "problem_sets/b_small.in"
+outputFile = "output/" + inputFile[13:-3]
+
+with open(inputFile, "r", errors="replace"):
+    pass
+
+with open(outputFile, "w", errors="replace"):
+    pass
